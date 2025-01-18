65b642a6217576b01abd9ba307a14f7a4d109aef
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2025-01-18 20:06:37 -0300

    feat: entity auditoriums criada

--- src/entities/Auditorio.ts
+++ src/entities/Auditorio.ts
@@ -1,5 +1,31 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('auditoriums')
 class Auditorio {
     
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    capacity: number;
+
+    @Column()
+    location: string;
+
+    @Column()
+    has_projector: boolean;
+
+    @Column()
+    has_sound_system: boolean;
+
+    @Column()
+    created_at: Date;
+
+    @Column()
+    updated_at: Date;
 }
 
 export default Auditorio;
