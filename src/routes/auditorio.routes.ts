f4d73dd533f8879ff1864b280a2bfa48565d0161
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2025-01-18 21:30:59 -0300

    feat: finalizadas todas as Routes para Auditorio

--- src/routes/auditorio.routes.ts
+++ src/routes/auditorio.routes.ts
@@ -113,4 +113,21 @@ auditorioRoutes.put('/:id', async (request, response) => {
     }
 })
 
+auditorioRoutes.delete('/:id', async (request, response) => {
+    try {
+        const id = Number(request.params.id)
+
+        const auditorioDeleted = await auditorioRepository.delete(id)
+
+        if(auditorioDeleted.affected === 0) {
+            response.status(404).json({ error: 'Auditorio não encontrado' })
+        } else {
+            response.status(204).json()
+        }
+    } catch (error) {
+        console.log(error)
+        response.status(500).json({ error : 'Erro ao deletar o auditorio'})
+    }
+})
+
 export default auditorioRoutes;
