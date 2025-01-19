89931b0d7b5ecf48fe75909d0d6f827cb1185087
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2025-01-18 21:03:28 -0300

    feat: route POST criada para Auditorios

--- src/routes/auditorio.routes.ts
+++ src/routes/auditorio.routes.ts
@@ -36,4 +36,42 @@ auditorioRoutes.get('/:id', async (request, response) => {
     }
 })
 
+auditorioRoutes.post('/', async (request, response) => {
+    try {
+        const auditorio = request.body
+
+        if(!auditorio.name) {
+            response.status(400).json({ error: 'Nome do auditório é obrigatório'})
+        } else if(!auditorio.capacity) {
+            response.status(400).json({ error: 'Capacidade do auditório é obrigatório'})
+        } else if(!auditorio.location) {
+            response.status(400).json({ error: 'Localização do auditório é obrigatório'})
+        } else if(!auditorio.has_projector) {
+            response.status(400).json({ error: 'Informação sobre o projetor é obrigatório'})
+        } else if(!auditorio.has_sound_system) { 
+            response.status(400).json({ error: 'Informação sobre o sistema de som é obrigatório'})
+        }
+        
+        else {
+            const newAuditorio = new Auditorio()
+    
+            newAuditorio.name = auditorio.name
+            newAuditorio.capacity = auditorio.capacity
+            newAuditorio.location = auditorio.location
+            newAuditorio.has_projector = auditorio.has_projector
+            newAuditorio.has_sound_system = auditorio.has_sound_system
+            newAuditorio.created_at = new Date()
+            newAuditorio.updated_at = new Date()
+
+            const savedProduct = await auditorioRepository.save(newAuditorio)
+    
+            response.status(201).json(savedProduct)
+            
+        }
+    } catch (error) {
+        console.log(error)
+        response.status(500).json({ error : 'Erro ao salvar o auditorio'})
+    }
+})
+
 export default auditorioRoutes;
